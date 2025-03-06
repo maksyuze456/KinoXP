f2adfb20e2637427876c62da0b656c277d613ee6
Maksym <maksym.045z@gmail.com>
2025-03-06 23:45:12 +0100

    CRUD, REST API for Booking

--- src/main/java/org/kinoxp/kinoxp/Controller/BookingController.java
+++ src/main/java/org/kinoxp/kinoxp/Controller/BookingController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RequestMapping("/booking")
 @RestController
 public class BookingController {
--- src/main/java/org/kinoxp/kinoxp/KinoXpApplication.java
+++ src/main/java/org/kinoxp/kinoxp/KinoXpApplication.java
@@ -1,7 +1,5 @@
 package org.kinoxp.kinoxp;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 

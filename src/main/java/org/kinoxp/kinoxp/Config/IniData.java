6a0cec23ba2e246a35f95be138203974c7d31ef8
Maksym <maksym.045z@gmail.com>
2025-03-10 15:25:06 +0100

    InitData for booking

--- src/main/java/org/kinoxp/kinoxp/Config/IniData.java
+++ src/main/java/org/kinoxp/kinoxp/Config/IniData.java
@@ -1,6 +1,8 @@
 package org.kinoxp.kinoxp.Config;
 
+import org.kinoxp.kinoxp.Entity.Booking;
 import org.kinoxp.kinoxp.Entity.Film;
+import org.kinoxp.kinoxp.Repository.BookingRepository;
 import org.kinoxp.kinoxp.Repository.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,6 +15,8 @@ public class IniData implements CommandLineRunner {
 
     @Autowired
     FilmRepository filmRepository;
+    @Autowired
+    BookingRepository bookingRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -41,6 +45,32 @@ public class IniData implements CommandLineRunner {
         filmRepository.save(film2);
         filmRepository.save(film3);
 
+        // InitData for Booking
+
+        Booking booking1 = new Booking();
+        booking1.setName("John");
+        booking1.setLastName("Johnson");
+        booking1.setPhone("24259570");
+        booking1.setAmount(2);
+        booking1.setShow(null);
+
+        Booking booking2 = new Booking();
+        booking2.setName("Brian");
+        booking2.setLastName("Davis");
+        booking2.setPhone("95349570");
+        booking2.setAmount(1);
+        booking2.setShow(null);
+
+        Booking booking3 = new Booking();
+        booking3.setName("Christian");
+        booking3.setLastName("Scott");
+        booking3.setPhone("34305701");
+        booking3.setAmount(2);
+        booking3.setShow(null);
+
+        bookingRepository.save(booking1);
+        bookingRepository.save(booking2);
+        bookingRepository.save(booking3);
 
     }
 }

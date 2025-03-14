c45a2d23542e3a5252ed401426cb731519c429b0
Maksym <maksym.045z@gmail.com>
2025-03-14 13:55:24 +0100

    *

--- src/main/java/org/kinoxp/kinoxp/Controller/BookingController.java
+++ src/main/java/org/kinoxp/kinoxp/Controller/BookingController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = "*")
 @RequestMapping("/booking")
 @RestController
 public class BookingController {
